958ae02128d7f2f2a24b89d99eccc67d38c9f891
showgo-koinuma <85728773+showgo-koinuma@users.noreply.github.com>
2023-09-29 00:35:22 +0900

    [add]サウンドをゲーム内で調整可能に、ゲーム中タイトルに戻れるように

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -10,6 +10,11 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
+        if (sceneName == "Title")
+        {
+            SceneManager.MoveGameObjectToScene(DDOLController.instance.gameObject, SceneManager.GetActiveScene());
+            Time.timeScale = 1;
+        }
         SceneManager.LoadScene(sceneName);
     }
 }
---
+++ Assets/Script/SoundVolumeManager.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundVolumeManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer _audioMixer;
+    [SerializeField] Slider _masterSlider;
+    [SerializeField] Slider _SESlider;
+    [SerializeField] TextMeshProUGUI _masterValueText;
+    [SerializeField] TextMeshProUGUI _SEValueText;
+
+    public void SetAudioMixerMaster()
+    {
+        _audioMixer.SetFloat("Master", _masterSlider.value * 100 - 80);
+        _masterValueText.text = (_masterSlider.value * 100).ToString();
+    }
+
+    //SE
+    public void SetAudioMixerSE()
+    {
+        _audioMixer.SetFloat("SE", _SESlider.value * 100 - 80);
+        _SEValueText.text = (_SESlider.value * 100).ToString();
+    }
+
+}
